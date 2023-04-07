e961c400a7c004bf624b4423307835c9a6c5216b
Crimson <121061785+Adri-nK@users.noreply.github.com>
2023-04-07 11:39:58 -0400

    alignment

--- RizzSwerve/src/main/java/frc/robot/Robot.java
+++ RizzSwerve/src/main/java/frc/robot/Robot.java
@@ -664,8 +664,11 @@ public class Robot extends TimedRobot {
 
     @Override
     public void autonomousPeriodic() {
-        // autoTopCone(); //balance
+        // autoMidBalance(); //balance
         autoTopAndBottom(); //not balance
+
+        // autoBlueBottomRedTop(); //testing
+        // autoBlueTopRedBottom(); //testing
     } 
 
     public void autoMidBalance() {
@@ -718,6 +721,84 @@ public class Robot extends TimedRobot {
         }else if (timerInterval_Auto(2.01, 3.50)){
             //riveSwerve_EncoderIf_FwdAndBwd(-0.3); //drive forwards to thingy
             swerveDrive(-0.3, 0, 0);
+            System.out.println("forward");
+        }else if (timerInterval_Auto(3.51, 6)){
+            swerveDrive(0,0,0);
+            armRotate_encoderIf_upAndDown(-1.80); //lift arm
+            System.out.println("arm u;p");
+        }else if (timerInterval_Auto(6.01, 8)){
+            armExtend_encoderIf_outAndIn(0.75); // extend arm
+            System.out.println("extend");
+        }else if (timerInterval_Auto(8.01, 8.5)){
+            dSolenoidClaw.set(Value.kForward); //open claw
+            System.out.println("drop");
+        }else if (timerInterval_Auto(8.51, 10)){
+            armExtend_encoderIf_outAndIn(0); //retract
+            dSolenoidClaw.set(Value.kReverse); //close claw
+            System.out.println("arm down");
+        }else if (timerInterval_Auto(10.01, 12)){
+            armRotate_encoderIf_upAndDown(-0.1); //lower arm
+            System.out.println("retract");
+        }else if (timerInterval_Auto(12.01, 15)){
+            driveSwerve_EncoderIf_FwdAndBwd(-4.3); //drive backwards past line
+            System.out.println("back it up");
+        }else { //STOP!!!
+            swerveDrive(0, 0, 0);
+            armRotate.tankDrive(0, 0);      
+            armTalonExtenstion.set(0);
+            System.out.println("stop");
+        }
+    }
+
+    public void autoBlueBottomRedTop() { //testing
+        // if (timerInterval_Auto(0, 0.50)){
+        //     System.out.println("encoders");
+        //     }
+        if (timerInterval_Auto(0, 2)){
+            driveSwerve_EncoderIf_turnOnSpot(Math.PI-0.01); // face nodes
+            System.out.println("turning");
+        }else if (timerInterval_Auto(2.01, 3.50)){
+            //riveSwerve_EncoderIf_FwdAndBwd(-0.3); //drive forwards to thingy
+            swerveDrive(-0.3, -0.3, 0);
+            System.out.println("forward");
+        }else if (timerInterval_Auto(3.51, 6)){
+            swerveDrive(0,0,0);
+            armRotate_encoderIf_upAndDown(-1.80); //lift arm
+            System.out.println("arm u;p");
+        }else if (timerInterval_Auto(6.01, 8)){
+            armExtend_encoderIf_outAndIn(0.75); // extend arm
+            System.out.println("extend");
+        }else if (timerInterval_Auto(8.01, 8.5)){
+            dSolenoidClaw.set(Value.kForward); //open claw
+            System.out.println("drop");
+        }else if (timerInterval_Auto(8.51, 10)){
+            armExtend_encoderIf_outAndIn(0); //retract
+            dSolenoidClaw.set(Value.kReverse); //close claw
+            System.out.println("arm down");
+        }else if (timerInterval_Auto(10.01, 12)){
+            armRotate_encoderIf_upAndDown(-0.1); //lower arm
+            System.out.println("retract");
+        }else if (timerInterval_Auto(12.01, 15)){
+            driveSwerve_EncoderIf_FwdAndBwd(-4.3); //drive backwards past line
+            System.out.println("back it up");
+        }else { //STOP!!!
+            swerveDrive(0, 0, 0);
+            armRotate.tankDrive(0, 0);      
+            armTalonExtenstion.set(0);
+            System.out.println("stop");
+        }
+    }
+
+    public void autoBlueTopRedBottom() { //testing
+        // if (timerInterval_Auto(0, 0.50)){
+        //     System.out.println("encoders");
+        //     }
+        if (timerInterval_Auto(0, 2)){
+            driveSwerve_EncoderIf_turnOnSpot(Math.PI-0.01); // face nodes
+            System.out.println("turning");
+        }else if (timerInterval_Auto(2.01, 3.50)){
+            //riveSwerve_EncoderIf_FwdAndBwd(-0.3); //drive forwards to thingy
+            swerveDrive(-0.3, 0.3, 0);
             System.out.println("forward");
         }else if (timerInterval_Auto(3.51, 6)){
             swerveDrive(0,0,0);
